f2c2f46a57ef8e2da790b712a7234128b8f29252
Jenny Sun <xmissj96@gmail.com>
2019-03-20 23:49:49 +0100

    Basics of visual representation done. Now need to take care of the 'Floating' disks.

--- Main/Main/Main.cpp
+++ Main/Main/Main.cpp
@@ -16,11 +16,21 @@
 using namespace std;
 
 // Global variables.
+static const int							MIN_SIZE = 3;
+static const int							MAX_SIZE = 7;
+static const int							TOWER_NAMES[3] = { 0, 1, 2 };
+static const string           PROGRAM_INTRO = "Welcome to ";
+static const string           PROGRAM_NAME = "Towers of Hanoi";
+static const string           PROGRAM_OUTPUT = "\nPlease enter the amount of disks in the stack (3 - 7). \n";
+static const string						PROGRAM_ERROR = "\nPlease pick number from 3 to 7. \n";
+
 int														diskCounter = -1;
 char													diskArray[] = { '1', '2', '3', '4', '5', '6', '7' };
 int														diskAmount;
 const int											amountOfPoles = 3;
-char													grid[3][7];
+char													grid[MIN_SIZE][MAX_SIZE];
+bool                          intro = false;
+int														moves = 0;
 
 void SetColor(int value) {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), value);
@@ -35,36 +45,31 @@ void delay(int numberOfSeconds) {
 	while (clock() < startTime + milliSeconds);
 }
 
-void visualisationHanoi(int height, char grid[][7], int destination, int diskNumber) {
+void visualisationHanoi(int height, int disk, int currentPosition, int nextPosition) {
 
-	// Fill the grid array.
-	for (int x = 0; x < height; x++)
+	for (int x = 0; x < amountOfPoles; x++)
 	{
-		for (int y = 0; y < amountOfPoles; y++)
+		for (int y = 0; y < height; y++)
 		{
-			grid[x][y] = '|';
-		}
-	}
+			if ((int)grid[x][y] - 48 == disk) {
 
-	// Insert disks.
-	for (int x = 0; x < height; x++)
-	{
-		diskCounter++;
-		for (int y = 0; y < amountOfPoles; y++)
-		{
-			grid[diskCounter][0] = diskArray[diskCounter];
-		}
-	}
+				// Save the disk
+				char movingDisk = grid[x][y];
 
-	// Reset counter.
-	diskCounter = -1; 
+				// Remove the old position
+				grid[x][y] = '|';
 
-	// Display the grid.
-	for (int x = 0; x < height; x++)
-	{
-		for (int y = 0; y < amountOfPoles; y++)
-		{
-			cout << "      " << grid[x][y];
+				// Move the disk to the next position
+				for (int nextPole = height; nextPole --> 0;)
+				{
+					if (grid[nextPole][nextPosition] == '|') {
+						grid[nextPole][nextPosition] = disk + '0';
+						break;
+					}
+				}
+
+			}
+			cout << "   " << grid[x][y] << "   ";
 		}
 		cout << endl;
 	}
@@ -72,37 +77,30 @@ void visualisationHanoi(int height, char grid[][7], int destination, int diskNum
 	printf("---------------------------\n");
 }
 
-void TowerOfHanoi(int number, int firstPillar, int midPillar, int lastPillar) {
+void TowerOfHanoi(int disk, int currentPosition, int midPillar, int nextPosition) {
 
-	if (number == 1) {
-		printf("Move disk %d from %d to %d \n", number, firstPillar, lastPillar);
+	if (disk == 1) {
+		moves++;
+		printf("Move disk %d from %d to %d \n", disk, currentPosition, nextPosition);
 		delay(10);
-		visualisationHanoi(diskAmount, grid, lastPillar, number);
+		visualisationHanoi(diskAmount, disk, currentPosition, nextPosition);
 		return;
 	}
 
 	// Makes recursive call to move number - 1 disks to auxiliary pilar.
-	TowerOfHanoi(number - 1, firstPillar, lastPillar, midPillar);
+	TowerOfHanoi(disk - 1, currentPosition, nextPosition, midPillar);
 
 	// Move the numberth disk from "A" to "B"
-	printf("Move disk %d from %d to %d \n", number, firstPillar, lastPillar);
+	printf("Move disk %d from %d to %d \n", disk, currentPosition, nextPosition);
 	delay(10);
-	visualisationHanoi(diskAmount, grid, lastPillar, number);
+	visualisationHanoi(diskAmount, disk, currentPosition, nextPosition);
 
 	// Move number - 1 disk from auxiliary pilar to destination pilar.
-	TowerOfHanoi(number - 1, midPillar, firstPillar, lastPillar);
+	TowerOfHanoi(disk - 1, midPillar, currentPosition, nextPosition);
 }
 
 int main()
 {
-	bool                          intro = false;
-	static const int							MIN_SIZE = 3;
-	static const int							MAX_SIZE = 7;
-	static const int							TOWER_NAMES[3] = { 0, 1, 2 };
-	static const string           PROGRAM_INTRO = "Welcome to ";
-	static const string           PROGRAM_NAME = "Towers of Hanoi";
-	static const string           PROGRAM_OUTPUT = "\nPlease enter the amount of disks in the stack (3 - 7). \n";
-	static const string						PROGRAM_ERROR = "\nPlease pick number from 3 to 7. \n";
 
 		// Program shows error, so long there is no valid number (minSize-maxSize).
 		do {
@@ -124,6 +122,16 @@ int main()
 				cerr << PROGRAM_ERROR << endl;
 			}
 		} while (diskAmount < MIN_SIZE || diskAmount > MAX_SIZE);
+
+
+		for (int x = 0; x < diskAmount; x++)
+		{
+			for (int y = 0; y < amountOfPoles; y++)
+			{
+				grid[x][y] = '|';
+			}
+			grid[x][0] = x + 1 + '0';
+		}
 
 		// A, B, C -> A is source, B is auxiliary and C is destination.
 		TowerOfHanoi(diskAmount, TOWER_NAMES[0], TOWER_NAMES[1], TOWER_NAMES[2]);
